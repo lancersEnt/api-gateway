e19605eac4b79162f304d0cb5572839413595b57
Ghassen Saaf <saafghassen@gmail.com>
2023-06-11 12:40:26 +0100

    apollo graphql federation gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'posts',
+              url: 'http://localhost:9119/graphql',
+            },
+            {
+              name: 'comments',
+              url: 'http://localhost:9229/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
 })
 export class AppModule {}
